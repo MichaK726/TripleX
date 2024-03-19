1482d174c01b2c1b6c638c20aeb8cd996825a2fc
MichaK726 <mm.koster@student.alfa-college.nl>
2024-03-19 19:00:51 +0100

    26 - 29

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
-void PrintIntroduction()
+void PrintIntroduction(int Difficulty)
 {
-    cout << "You are a secret agent breaking into a secure server room..." << endl;
-    cout << "Enter the correct code to continue..." << endl;
+    cout << "\n\nYou are a secret agent breaking into a level " << Difficulty;
+    cout << " secure server room...\nEnter the correct code to continue...\n\n";
 }
 
-void PlayGame()
+bool PlayGame(int Difficulty)
 {
-    PrintIntroduction();
+    PrintIntroduction(Difficulty);
 
     // Declare 3 number code
-    const int CodeA = 4;
-    const int CodeB = 3;
-    const int CodeC = 2;
+    const int CodeA = rand() % Difficulty + Difficulty;
+    const int CodeB = rand() % Difficulty + Difficulty;
+    const int CodeC = rand() % Difficulty + Difficulty;
 
     const int CodeSum = CodeA + CodeB + CodeC;
     const int CodeProduct = CodeA * CodeB * CodeC;
 
     // Print CodeSum and CodeProduct to the terminal
-    cout << endl;
-    cout << "There are 3 numbers in the code" << endl;
+    cout << "There are 3 numbers in the code \n";
     cout << "The codes add up to: " << CodeSum << endl;
     cout << "The numbers multiply to: " << CodeProduct << endl;
 
@@ -35,16 +35,36 @@ void PlayGame()
     // Check if the players guess is correct
     if (GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
-        cout << "You win!";
+        cout << "\n*** Well done agent! You have extracted a file! Keep going! ***";
+        return true;
     }
     else
     {
-        cout << "You lose!";
+        cout << "\n*** You entered the wrong code! Careful agent! Try again! ***";
+        return false;
     }
 }
 
 int main()
 {
-    PlayGame();
+    srand(time(NULL)); // Create new random sequence based on time of day
+
+    int LevelDifficulty = 1;
+    int const MaxDifficulty = 5;
+
+    while (LevelDifficulty <= MaxDifficulty) // Loop the game until all levels completed
+    {
+        bool bLevelComplete = PlayGame(LevelDifficulty);
+        cin.clear();  // Clears any errors related to input
+        cin.ignore(); // Discards the buffer
+
+        if (bLevelComplete)
+        {
+            LevelDifficulty++;
+        }
+    }
+
+    cout << "\n*** Great work agent! You got all the files! Now get out of there! ***";
+
     return 0;
 }
